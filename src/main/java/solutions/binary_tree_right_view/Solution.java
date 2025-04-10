9e63e28fefa5c9c0b5e4ec3cd2edb1c489058537
TeodorRussu <teodor_russu@yahoo.com>
2025-04-10 22:56:01 +0200

    Binary tree view from the right optimized

--- src/main/java/solutions/binary_tree_right_view/Solution.java
+++ src/main/java/solutions/binary_tree_right_view/Solution.java
@@ -28,33 +28,32 @@ class TreeNode {
 public class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> output = new ArrayList<>();
-
         if (root == null) {
             return output;
         }
+        Deque<TreeNode> currentLevel = new LinkedList<>();
 
-        Deque<TreeNode> current = new LinkedList<>();
-        Deque<TreeNode> next = new LinkedList<>();
-        current.add(root);
+        currentLevel.add(root);
         output.add(root.val);
 
-        while (!current.isEmpty()) {
-            while (!current.isEmpty()) {
-                TreeNode nextElement = current.poll();
+        while (!currentLevel.isEmpty()) {
+            int currentLevelSize = currentLevel.size();
+            TreeNode rightMostOnThisLevel = null;
+            for (int i = 0; i<currentLevelSize; i++) {
+                TreeNode nextElement = currentLevel.poll();
                 if (nextElement.left != null) {
-                    next.add(nextElement.left);
+                    currentLevel.add(nextElement.left);
+                    rightMostOnThisLevel = nextElement.left;
                 }
                 if (nextElement.right != null) {
-                    next.add(nextElement.right);
+                    currentLevel.add(nextElement.right);
+                    rightMostOnThisLevel = nextElement.right;
                 }
             }
-            TreeNode mostRightNode = next.peekLast();
-            if (mostRightNode != null) {
-                output.add(mostRightNode.val);
-            }
 
-            current = next;
-            next = new LinkedList<>();
+            if (rightMostOnThisLevel != null) {
+                output.add(rightMostOnThisLevel.val);
+            }
         }
         return output;
     }
